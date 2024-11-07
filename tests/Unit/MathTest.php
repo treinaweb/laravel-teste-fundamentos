d3ae750c30453fe107e38cd8f02c7f362a624416
Elton Fonseca <elton869@gmail.com>
2024-11-07 16:06:14 -0300

    02 - Criando e executando o primeiro teste

---
+++ tests/Unit/MathTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class MathTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_verifica_se_soma_dois_numeros_corretamente(): void
+    {
+        $this->assertTrue(true);
+    }
+}
