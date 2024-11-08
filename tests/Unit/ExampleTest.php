8ee23f0cdc54183b3df81895679a8d40cc71315e
Elton Fonseca <elton869@gmail.com>
2024-11-08 10:59:22 -0300

    Conhecendo os asserts array

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -9,8 +9,20 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_that_true_is_true(): void
+    public function test_array_asserts(): void
     {
-        $this->assertTrue(true);
+        $nomes = ['joão', 'maria', 'pedro'];
+        $this->assertContains('maria', $nomes);
+        $this->assertNotContains('elton', $nomes);
+
+        $this->assertCount(3, $nomes);
+
+        $pessoa = ['nome' => 'joão', 'idade' => 25];
+        $this->assertArrayHasKey('nome', $pessoa);
+        $this->assertArrayNotHasKey('cpf', $pessoa);
+
+        $nomes2 = ['joão', 'maria', 'pedro'];
+        $this->assertEquals($nomes, $nomes2);
+        $this->assertNotEquals($nomes, $pessoa);
     }
 }
