64ad01251d5d0f2ba92ad76cc7ed7bcb66a39d9e
Elton Fonseca <elton869@gmail.com>
2024-11-08 11:22:05 -0300

    06 - Conhecendo os asserts para trabalhar com objetos

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,8 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
+    public $name;
+
     /**
      * The attributes that are mass assignable.
      *
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Unit;
 
+use App\Models\User;
 use PHPUnit\Framework\TestCase;
+
+use function PHPUnit\Framework\assertNull;
 
 class ExampleTest extends TestCase
 {
@@ -24,5 +27,22 @@ class ExampleTest extends TestCase
         $nomes2 = ['joão', 'maria', 'pedro'];
         $this->assertEquals($nomes, $nomes2);
         $this->assertNotEquals($nomes, $pessoa);
+    }
+
+    function test_object_asserts(): void
+    {
+        $user = new User();
+        $this->assertInstanceOf(User::class, $user);
+
+        $user->name = 'elton';
+        $this->assertObjectHasProperty('name', $user);
+        $this->assertEquals('elton', $user->name);
+
+        $this->assertNull($user->idade);
+
+        $user2 = new User();
+        $user2->name = 'elton';
+
+        $this->assertEquals($user, $user2);
     }
 }
