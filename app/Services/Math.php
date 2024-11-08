24691437e82d04511b8f7585b4a541ba0bb32e0f
Elton Fonseca <elton869@gmail.com>
2024-11-08 12:40:38 -0300

    08 - Conhecendo os métodos do ciclo

--- app/Services/Math.php
+++ app/Services/Math.php
@@ -8,4 +8,9 @@ class Math
     {
         return $n1 + $n2;
     }
+
+    public function subtrair(int $n1, int $n2): int
+    {
+        return $n1 - $n2;
+    }
 }
--- tests/Unit/Services/MathTest.php
+++ tests/Unit/Services/MathTest.php
@@ -7,18 +7,50 @@ use PHPUnit\Framework\TestCase;
 
 class MathTest extends TestCase
 {
+    private static ?Math $mathService;
+
+    public static function setUpBeforeClass(): void
+    {
+        static::$mathService = app(Math::class);
+    }
+
+    // protected function setUp(): void
+    // {
+    //     parent::setUp();
+
+    //     $this->mathService = app(Math::class);
+    // }
+
     /**
      * A basic unit test example.
      */
     public function test_verifica_se_soma_dois_numeros_corretamente(): void
     {
-        //arange
-        $mathService = app(Math::class);
-
         //act
-        $result = $mathService->somar(1, 2);
+        $result = static::$mathService->somar(1, 2);
 
         //assert
         $this->assertEquals(3, $result);
+    }
+
+    public function test_verifica_se_subtrai_dois_numeros_corretamente(): void
+    {
+        //act
+        $result = static::$mathService->subtrair(9, 4);
+
+        //assert
+        $this->assertEquals(5, $result);
+    }
+
+    // protected function tearDown(): void
+    // {
+    //     parent::tearDown();
+
+    //     $this->mathService = null;
+    // }
+
+    public static function tearDownAfterClass(): void
+    {
+        static::$mathService = null;
     }
 }
