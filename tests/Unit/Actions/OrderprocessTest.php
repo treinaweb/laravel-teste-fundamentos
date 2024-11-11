83cf65c82d74448dacdc385d7f2f2e7046fc92e6
Elton Fonseca <elton869@gmail.com>
2024-11-11 14:45:43 -0300

    13 - Usando o container para obter as instancias

--- tests/Unit/Actions/OrderprocessTest.php
+++ tests/Unit/Actions/OrderprocessTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Actions;
 
 use App\Actions\OrderProcess;
+use App\Interfaces\PayPalInterface;
 use App\Services\PayPal;
 use PHPUnit\Framework\TestCase;
 use Tests\Unit\TestDoubles\PayPalDummy;
@@ -15,8 +16,10 @@ class OrderprocessTest extends TestCase
     public function test_verifica_se_consegue_criar_um_pedido_com_secesso(): void
     {
         //Arrange
-        $paypal = new PayPalDummy();
-        $orderProcess = new OrderProcess($paypal);
+        // $paypal = new PayPalDummy();
+        // $orderProcess = new OrderProcess($paypal);
+        app()->bind(PayPalInterface::class, PayPalDummy::class);
+        $orderProcess = app(OrderProcess::class);
 
         //Act
         $retorno = $orderProcess->execute();
