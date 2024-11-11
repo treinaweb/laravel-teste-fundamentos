af12a41bfffdef94c4990b8adf8a8ba74a408aa7
Elton Fonseca <elton869@gmail.com>
2024-11-11 18:27:02 -0300

    18 - Conhecendo o Mockery

--- tests/Unit/Actions/OrderProcessMockTest.php
+++ tests/Unit/Actions/OrderProcessMockTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Actions;
 
 use App\Actions\OrderProcess;
 use App\Interfaces\PayPalInterface;
+use Mockery;
 use PHPUnit\Framework\Constraint\IsType;
 use PHPUnit\Framework\TestCase;
 
@@ -12,7 +13,7 @@ class OrderProcessMockTest extends TestCase
     /**
      * A basic unit test example.
      */
-    public function test_verifica_se_o_pagamento_foi_realizado_apenas_uma_vez(): void
+    public function test_verifica_se_o_pagamento_foi_realizado_apenas_uma_vez_phpunit(): void
     {
         //arrange
         $paypal = $this->createMock(PayPalInterface::class);
@@ -22,6 +23,25 @@ class OrderProcessMockTest extends TestCase
                 ->willReturn(true);
 
         $orderProcess = new OrderProcess($paypal);
+
+        //act
+        $retorno = $orderProcess->execute();
+
+        //asserts
+        $this->assertEquals('Order processed', $retorno);
+    }
+
+    public function test_verifica_se_o_pagamento_foi_realizado_apenas_uma_vez_mockery(): void
+    {
+        //arrange
+        $paypal = Mockery::mock(PayPalInterface::class);
+        $paypal->shouldReceive('paymentProcess')
+                ->once()
+                ->with(Mockery::type('int'))
+                ->andReturn(true);
+
+        app()->instance(PayPalInterface::class, $paypal);
+        $orderProcess = app(OrderProcess::class);
 
         //act
         $retorno = $orderProcess->execute();
