17c28f00156671f1b48f3aa716f95e61d82ba204
Elton Fonseca <elton869@gmail.com>
2024-11-12 17:08:40 -0300

    21 - Testando dados invalidos

--- tests/Unit/Services/SpreadsheetServiceTest.php
+++ tests/Unit/Services/SpreadsheetServiceTest.php
@@ -31,4 +31,36 @@ class SpreadsheetServiceTest extends TestCase
         $this->assertCount(2, $validItems);
         $this->assertEquals($data, $validItems);
     }
+
+    public function test_processa_somente_dados_validos(): void
+    {
+        //Arrange
+        $data = [
+            ['product_code' => 'VALID1', 'quantity' => 10],
+            ['product_code' => null, 'quantity' => 5],
+            ['product_code' => null, 'quantity' => null],
+            ['product_code' => 'INVALID1', 'quantity' => null],
+            ['product_code' => 'INVALID2', 'quantity' => 0]
+        ];
+
+        $import = Mockery::mock();
+        $import->shouldReceive('import')
+                ->with('caminho/do/arquivo')
+                ->andReturn($data);
+        app()->instance('importer', $import);
+
+        $service = app(SpreadsheetService::class);
+
+        //Act
+        $validItems = $service->processSpreadsheet('caminho/do/arquivo');
+
+        //Asserts
+        $this->assertCount(1, $validItems);
+        $this->assertEquals(
+            [
+                ['product_code' => 'VALID1', 'quantity' => 10],
+            ], 
+            $validItems
+        );
+    }
 }
